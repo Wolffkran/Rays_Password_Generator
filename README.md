50ade8608df77a78e661007b3979b1f10f6e916b
Wolffkran <liebherr2008@live.com>
2023-09-10 03:31:05 -0400

    Added the README and changed the CSS

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Password Generator Starter Code
+# Password Generator Challenge
+
+## Description
+
+The purpose of this project was to create a website with the capability of generating passwords for personal use.
+Constructing this helped me learn more about arrays and functions.
+
+## Installation
+
+
+
+## Usage
+
+![Application Screenshot](assets/images/screenshot.png)
+
+## License
+
+© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Get references to the #generate element and checkboxes
 var generateBtn = document.querySelector("#generate");
+var lowercaseCheckbox = document.querySelector("#lowercase");
+var uppercaseCheckbox = document.querySelector("#uppercase");
+var numericCheckbox = document.querySelector("#numeric");
+var specialCheckbox = document.querySelector("#special");
 
 // Write password to the #password input
 function writePassword() {
@@ -10,7 +11,38 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+// Function to generate a password based on selected criteria
+function generatePassword() {
+  // Define character sets based on selected criteria
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+
+  // Initialize the characters variable to include uppercase and lowercase letters
+  var characters = lowercaseChars + uppercaseChars;
+
+  // Check which checkboxes are selected and add corresponding character sets
+  if (numericCheckbox.checked) {
+    characters += numericChars;
+  }
+  if (specialCheckbox.checked) {
+    characters += specialChars;
+  }
+
+  // Get the desired password length from the length input
+  var length = parseInt(document.querySelector("#length").value);
+
+  // Generate the password
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomIndex);
+  }
+
+  return password;
 }
 
 // Add event listener to generate button
