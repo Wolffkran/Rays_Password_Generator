2bad807cfbaf33d0f12d5b03d813f310a2ffcf05
Wolffkran <liebherr2008@live.com>
2023-09-13 23:32:05 -0400

    Added prompts, alerts, and confirms to the code.

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-// Get references to the #generate element and checkboxes
-var generateBtn = document.querySelector("#generate");
-var lowercaseCheckbox = document.querySelector("#lowercase");
-var uppercaseCheckbox = document.querySelector("#uppercase");
-var numericCheckbox = document.querySelector("#numeric");
-var specialCheckbox = document.querySelector("#special");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
 // Function to generate a password based on selected criteria
 function generatePassword() {
+  // Prompt for password length
+  var lengthInput = prompt("Enter password length (8-128):");
+  var length = parseInt(lengthInput);
+
+  // Check if the input is a valid number and within the specified range
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid input! Password length must be between 8 and 128 characters.");
+    return "";
+  }
+
+  // Confirm character types
+  var lowercase = confirm("Include lowercase letters?");
+  var uppercase = confirm("Include uppercase letters?");
+  var numeric = confirm("Include numeric characters?");
+  var special = confirm("Include special characters?");
+
   // Define character sets based on selected criteria
   var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numericChars = "0123456789";
-  var specialChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+  var specialChars = "!@#$%^&*()_+~`|}{[]\\:;?><,./-=";
 
   // Initialize the characters variable to include uppercase and lowercase letters
   var characters = lowercaseChars + uppercaseChars;
 
-  // Check which checkboxes are selected and add corresponding character sets
-  if (numericCheckbox.checked) {
+  // Check which confirmations are true and add corresponding character sets
+  if (numeric) {
     characters += numericChars;
   }
-  if (specialCheckbox.checked) {
+  if (special) {
     characters += specialChars;
   }
-
-  // Get the desired password length from the length input
-  var length = parseInt(document.querySelector("#length").value);
 
   // Generate the password
   var password = "";
@@ -45,5 +43,16 @@ function generatePassword() {
   return password;
 }
 
-// Add event listener to generate button
+// Get references to the #generate element and checkboxes
+var generateBtn = document.querySelector("#generate");
+
+// Add an event listener to the Generate Password button
 generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
